f918fd266dc025222b43f0d28a0349d4cbf77b81
marcel-codett <uwakwemgodspower99@gmail.com>
2023-01-20 05:35:29 -0800

    feat: adding currency-formatter

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import { StarIcon } from '@heroicons/react/solid'
+import Currency from 'react-currency-formatter'
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -26,8 +27,8 @@ const Product = ({id, title, price, description, category, image }) => {
     </div>
 
     <p className='text-xs my-2 line-clamp-2'>{description}</p>
-    <div className='mb-5'>
-      <p>$1.00</p>
+    <div className='mb-5 text-extrabold'>
+      <Currency quantity={price} currency='GBP' />
     </div>
     {hasPrime && (
       <div className='flex items-center space-x-2 -mt-5'>
